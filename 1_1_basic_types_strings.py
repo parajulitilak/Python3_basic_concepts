7514dfca6d94cecfafac03bbbf20b88ecce4776c
Tilak Parajuli <128390170+tilakparajuli0001@users.noreply.github.com>
2023-03-22 23:31:15 +0545

    Add files via upload

---
+++ 1_1_basic_types_strings.py
@@ -0,0 +1,5 @@
+print("Hello Tilak")
+print('Hello Tilak again!!')
+print("don't do that !!") #if we use single quotation here error occurs,to avoid error wrap it inside double quote
+print('She said "i wan`t this"  ')
+print('My name is tilak\'s')
