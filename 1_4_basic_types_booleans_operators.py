6c87625506fbf6a317d623fc9e2bad105e115962
Tilak Parajuli <128390170+tilakparajuli0001@users.noreply.github.com>
2023-03-24 00:21:09 +0545

    Add files via upload
    basic boolean operators manipulation in python

---
+++ 1_4_basic_types_booleans_operators.py
@@ -0,0 +1,21 @@
+#it`s a programming concept in general.
+#booleand are either TRUE or FALSE
+print(5==5) #this results True
+
+print(5==4)  #this results False
+
+#we can also do it like this in python
+
+print(5 is 4) #returns False
+print(5 is 5)  #returns True
+
+print(5 is not 5) #returns False
+
+#we can do this with strings too
+
+print("tilak" is "tilak")
+
+print(True is True) #returns true
+print(True is False) #returns false
+
+print("True" is str(True))
