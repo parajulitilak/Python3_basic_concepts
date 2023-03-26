b663cf89c2bc9736f0735c88611263d9376b0688
Tilak Parajuli <128390170+tilakparajuli0001@users.noreply.github.com>
2023-03-26 17:27:56 +0545

    Add files via upload

---
+++ 1_9_user_defined_functions.py
@@ -0,0 +1,13 @@
+#always follow pip guidelines
+#use snake case while creating function in python and camel case for class name
+def my_function():
+    #leave 4 spaces in the left side for identitation
+    print("i`m learning python 3")
+    print("This is my function")
+     
+my_function()
+'''
+output:
+        i`m learning python 3
+        This is my function
+'''
