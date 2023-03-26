a4b0129a41ee2167a0662a549fcbfb1699dcb72c
Tilak Parajuli <128390170+tilakparajuli0001@users.noreply.github.com>
2023-03-26 15:45:19 +0545

    Add files via upload
    basic list manipulation in python3

---
+++ 1_5_lists.py
@@ -0,0 +1,12 @@
+mylist=["movies","songs","python"]
+print("i like ",mylist[0])
+
+#the above print line will print i like movies.
+
+mynewlist=["movies",23,"songs"]
+print("i`m",mynewlist[1],"years old")
+
+#the above line will print i`m 23 years old
+
+print("i`m ",["python",21,"hello"][1],"years old.")
+#the above line will print i`m 21 years old.
