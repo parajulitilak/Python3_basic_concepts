70d889005bf455237b1149afeb7abcacc6be5d2c
Tilak Parajuli <128390170+tilakparajuli0001@users.noreply.github.com>
2023-03-26 17:36:56 +0545

    Add files via upload

---
+++ 1_10_adding_arguments_to_function.py
@@ -0,0 +1,13 @@
+'''
+we use arguments in function to make it more dynamic.
+'''
+def my_function(str1,str2):
+    print(str1)
+    print(str2)
+     
+str1="Hello folks!!!"
+str2="I`m Tilak Parajuli"
+my_function(str1,str2)
+
+my_function("\n","\n")
+my_function("Hello","World!!")
