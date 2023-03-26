62d2fc2f25047387850f96c0a6dae84dc2986e66
Tilak Parajuli <128390170+tilakparajuli0001@users.noreply.github.com>
2023-03-26 16:34:40 +0545

    Add files via upload

---
+++ 1_7_variables.py
@@ -0,0 +1,19 @@
+#setting a variable doesn`t have any response.
+greeting="Hello World"
+print(greeting) #this line will print Hello World
+
+greeting=greeting.split(" ")[0]
+print(greeting) #this will print only Hello
+
+print(greeting,"Tilak")
+
+#if we just want to store that into variable do like this
+greeting=greeting +" someone else."
+print(greeting) #this line will print Hello someone else
+
+number1=6
+number2=8
+
+print(number1*number2-number1+number2+11)
+
+check=True
