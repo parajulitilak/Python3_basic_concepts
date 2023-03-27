af28b0cb54265d579abd760d9361e342886d00bd
Tilak Parajuli <128390170+tilakparajuli0001@users.noreply.github.com>
2023-03-28 00:09:10 +0545

    Add files via upload

---
+++ 1_12_infinite_arguments.py
@@ -0,0 +1,16 @@
+def person_people(*person):
+    for people in person:
+        print("The person name is ",people,".")
+
+
+person_people("Tilak","Elon","Shayam","Sita","hari","rawaan")
+
+'''
+#output
+The person name is  Tilak .
+The person name is  Elon .
+The person name is  Shayam .
+The person name is  Sita .
+The person name is  hari .
+The person name is  rawaan .
+'''
